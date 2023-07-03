f6b027e7b00a81bfcaf5d4ab21a3d012678a5d95
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-07-03 23:47:26 +0800

    Update xieyuen.py

--- 爬/xieyuen.py
+++ 爬/xieyuen.py
@@ -19,17 +19,17 @@ class Information:
 
     __license = 'MIT License\n\nCopyright (c) 2023 HIM049\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.'
 
-    def __init__(self, _any): # 初始化，没得说
-        self.any = _any
+    def __init__(self, _info): # 初始化，没得说
+        self._info = _info
         self.help = self.Help()
     
 
     # LICENSE 开源协议
     def LICENSE():
-        print(self.__license)
+        print(Information.__license)
 
     def license():
-        self.LICENSE()
+        Information.LICENSE()
 
 
     def author():
@@ -41,17 +41,20 @@ class Information:
 
 
     def copyright_info():
-        self.author()
-        print('')
-        print('LICENSE：')
-        self.LICENSE()
+        Information.author()
+        print('LICENSE：\n')
+        Information.LICENSE()
 
 class Help:
 
-    def __init__(self, _any): # 初始化，没得说
-        self.any = _any
+    def __init__(self, _help): # 初始化，没得说
+        self._help = _help
         self.Crawler = self.Crawler()
-        self.Tools = Tools()
+        self.Tools = self.Tools()
+
+
+    def printHelpMassage(_help_massage):
+        print(_help_massage)
 
 
     def list():
@@ -68,8 +71,8 @@ class Help:
 
 
     class List:
-        def __init__(self, _any):
-            self.any = _any
+        def __init__(self, _list):
+            self.list = _list
         
         def Crawler():
             pass
@@ -80,35 +83,51 @@ class Help:
 
 
     def crawler():
-        print('一堆爬虫\n使用爬虫记得调用 main_program\n\n现有:\n    1. Music\n        - 音乐爬虫，支持网易云、QQ等平台\n   2. Picture\n        - 也许是最简单的图片爬虫了')
+        _help_massage = '一堆爬虫\n使用爬虫记得调用 main_program\n\n现有:\n    1. Music\n        - 音乐爬虫，支持网易云、QQ等平台\n   2. Picture\n        - 也许是最简单的图片爬虫了'
+        Help.printHelpMassage(_help_massage)
 
 
     def help():
-        print('这是这个python包的帮助页面')
-        print('')
+        _help_massage = '这是这个python包的帮助页面'
+        Help.printHelpMassage(_help_massage)
+
 
 
     class Crawler:
 
-        def __init__(self, _any): # 初始化，没得说
-            self.any = _any
+        def __init__(self, _crawler): # 初始化，没得说
+            self._crawler = _crawler
 
 
         def main_program():
-            print('爬虫的主程序，要使用的话请调用它')
+            _help_massage = '爬虫的主程序，要使用的话请调用它'
+            Help.printHelpMassage(_help_massage)
 
 
         def music():
-            print('音乐爬虫awa\n使用爬虫请调用`Crawler.Music.main_program()`这个函数awa\n\n支持:\n    1. 网易云\n    2.QQ\n    3.酷狗\n    4.酷我:kuwo\n    5.百度:baidu\n    6.喜马拉雅:ximalaya')
+            _help_massage = '音乐爬虫awa\n使用爬虫请调用`Crawler.Music.main_program()`这个函数awa\n\n支持:\n    1. 网易云\n    2.QQ\n    3.酷狗\n    4.酷我:kuwo\n    5.百度:baidu\n    6.喜马拉雅:ximalaya'
+            Help.printHelpMassage(_help_massage)
 
 
         def picture():
-            print('图片爬虫awa\n使用爬虫请调用`Crawler.Picture.main_program()`这个函数awa')
+            _help_massage = '图片爬虫awa\n使用爬虫请调用`Crawler.Picture.main_program()`这个函数awa'
+            Help.printHelpMassage(_help_massage)
+
 
     def tools():
-        print('简单的小工具')
+        _help_massage = '简单的小工具'
+        Help.printHelpMassage(_help_massage)
 
-    class Tools: pass
+
+    class Tools:
+        
+        def __init__(self, _tools):
+            self._tools = _tools
+
+
+        def chinese_count():
+            _help_massage = '计算汉字数量并记录汉字位置\n返回值像这样：\n\n[{"汉字数量": 11,"字母数量": 45,"非字母汉字数量": 14},[1, 9, 19, 81 ...]]\n后面的列表是汉字所在字符串的索引'
+            Help.printHelpMassage(_help_massage)
 
 class Crawler: 
 
@@ -123,8 +142,8 @@ class Crawler:
                 - 也许是最简单的图片爬虫了
     '''
 
-    def __init__(self, _any): # 初始化，没得说
-        self.any = _any
+    def __init__(self, _crawler): # 初始化，没得说
+        self._crawler = _crawler
         self.music = self.Music()
         self.picture = self.Picture()
 
@@ -207,7 +226,7 @@ class Crawler:
                 case _:
 
                     print(f"【ERROR】\n无法识别到你输入的 '{ platfrom }' 平台")
-                    __check = int(input('请选择操作：\n[1]重新输入参数\n[2]使用默认值'))
+                    __check = int(input('请选择操作：\n [1]重新输入参数\n [2]使用默认值'))
                     print("-------------------------------------------------------")
                     if __check == 1:
                         Crawler.Music.get_music_platfrom()
@@ -328,69 +347,204 @@ class Tools:
     """
 
 
-    def __init__(self, _any): # 初始化，没得说
-        self.any = _any
+    def __init__(self, _tools): # 初始化，没得说
+        self._tools = _tools
         self.list_sort = self.ListSort()
-        self.sys_cmd = self.Sys_cmd()
-        # self.picture = self.Picture()
+        self.sysCmd = self.SysCmd()
+        self.math = self.Math()
 
 
     def help():
         Help.tools()
 
 
-    def is_exist_chinese(string):
+    def isExistChinese(string):
 
         '''
             检测字符串内是否存在汉字
         '''
 
         for char in string:
-
             if u'\u4e00' <= char <= u'\u9fa5':  # 判断是否是汉字
                 return True
 
         return False
 
 
-    def chinese_count(string):
+    def chineseCount(string):
 
         '''
             计算汉字数量并记录汉字位置
             返回值像这样：
 
-            [{"汉字数量": 11,"字母数量": 45,"非字母汉字数量": 14},[1, 9, 19, 81 ...]]
-        '''
+            >>> chinese_count('加油华为 加油China')
+            {
+                'count': {
+                    'chinese': 6, 
+                    'alpha': 5, 
+                    '_': 1
+                }, 
+                'chineseIndex': [0, 1, 2, 3, 5, 6]
+            }
 
-        result_1 = {
-            "汉字数量": 0,
-            "字母数量": 0,
-            "非字母汉字数量": 0
-        }    #[["汉字数量", "字母数量", "非字母汉字数量"], [0, 0, 0]]
-        result_2 = []
-        i = 0
+            后面的列表是汉字所在字符串的索引
+        '''
+        
+        __result = {
+            'count': {
+                'chinese': 0,
+                'alpha': 0,
+                '_': 0
+            },
+            'chineseIndex': []
+        }
+        __index = 0
 
         for char in string:
 
             if u'\u4e00' <= char <= u'\u9fa5':  # 判断是否是汉字，在isalpha()方法之前判断
 
-                result_1["汉字数量"] += 1
-                result_2.append(i)
+                __result['count']['chinese'] += 1
+                __result['chineseIndex'].append(__index)
 
             elif char.isalpha():  # ！汉字也返回 true
 
-                result_1["字母数量"] += 1
+                # __result_1["字母数量"] += 1
+                __result['count']['alpha'] += 1
 
             else:
 
-                result_1["非字母汉字数量"] += 1
+                # __result_1["非字母汉字数量"] += 1
+                __result['count']['_'] += 1
 
-            i += 1
-        result=[result_1, result_2]
-        return result
+            __index += 1
+        
+        return __result
 
 
-    def list_deduplication(_list): #List deduplication
+    def count(string):
+
+        '''
+            计算字符数量并记录字符位置
+            返回值像这样：
+
+            >>> count('加油华为 加油China')
+            {
+                'count': {
+                    'chinese': 6,
+                    'alpha': {
+                        'all': 5, 
+                        'lowercase': 4, 
+                        'uppercase': 1, 
+                        '_': 0
+                    },
+                    ' ': 1,
+                    '_': 0},
+                'index': {
+                    'chinese': [0, 1, 2, 3, 5, 6],
+                    'alpha':像长{
+                        'all': [7, 8, 9, 10, 11],
+                        'lowercase': [8, 9, 10, 11],
+                        'uppercase': [7],
+                        '_': []
+                    },
+                    ' ': [4],
+                    '_': []
+                }
+            }
+
+            后面的列表是汉字所在字符串的索引
+            返回值说明：
+            'count': 数量
+            'index': 索引
+            'chinese': 汉字
+            'alpha': 字母
+                'all': 全部字母
+                'lowercase': 小写字母
+                'uppercase': 大写字母
+            ' ': 空格 spaxce
+            '_': 其他(通配符)
+
+        '''
+
+        __result = {
+            'count': { # 数量
+                'chinese': 0, # 汉字
+                'alpha': { # 字母
+                    'all': 0, # 全部
+                    'lowercase': 0, # 小写
+                    'uppercase': 0, # 大写
+                    '_': 0 # 其他
+                },
+                ' ': 0, # 空格 space
+                '_': 0 # 其他
+            },
+            'index': { # 索引，内容同上
+                'chinese': [],
+                'alpha': {
+                    'all': [],
+                    'lowercase': [],
+                    'uppercase': [],
+                    '_': []
+                },
+                ' ': [],
+                '_': []
+            }
+        }
+
+        for __index in range(len(string)):
+            char = string[__index]
+
+            if u'\u4e00' <= char <= u'\u9fa5':  # 判断是否是汉字，在isalpha()方法之前判断
+                __result['count']['chinese'] += 1
+                __result['index']['chinese'].append(__index)
+
+            elif char.isalpha():  # ！汉字也返回 true
+                __result['count']['alpha']['all'] += 1
+                __result['index']['alpha']['all'].append(__index)
+
+                if char.islower(): # 小写字母
+                    __result['count']['alpha']['lowercase'] += 1
+                    __result['index']['alpha']['lowercase'].append(__index)
+
+                elif char.isupper(): # 大写字母
+                    __result['count']['alpha']['uppercase'] += 1
+                    __result['index']['alpha']['uppercase'].append(__index)
+
+                else:
+                    __result['count']['alpha']['_'] += 1
+                    __result['index']['alpha']['_'].append(__index)
+
+            elif char == ' ': # 空格
+                __result['count'][' '] += 1
+                __result['index'][' '].append(__index)
+
+            else: # 其他
+                __result['count']['_'] += 1
+                __result['index']['_'].append(__index)
+
+        # 删除字典里值为 0 对应的索引
+        for i in __result['count']:
+
+            if isinstance(__result['count'][i], dict):
+            
+                for j in __result['count'][i]:
+                    if __result['count'][i][j] == 0:
+                        del __result['index'][i][j]
+                        if j == 'all':
+                            del __result['index'][i]
+                            __result['count'][i] = 0
+                            continue
+            
+            else:
+            
+                if __result['count'][i] == 0:
+                    del __result['index'][i]
+
+        return __result
+
+
+    def listDedup(_list): # List dedup
 
         '''
             给列表不打乱顺序地去重
@@ -404,13 +558,55 @@ class Tools:
 
         return result
 
+
+    class Math:
+
+        def __init__(self, _math): # 初始化，没得说
+            self._math = _math
+            self.constant = self.Constant()
+        
+
+        def sum(_expressions,_start,_stop):
+            
+            '''
+                sum(关于x的表达式(需要定义成函数/方法), 起始值, 终值)
+                起始值必须大于终值
+            '''
+            
+            __sum = 0
+            x = _start
+
+            pass
+            while x <= _stop:
+                __sum += _expressions(x)
+                x += 1
+            
+            return __sum
+
+        
+        class Constant:
+
+            def __init__(self, _c): # 初始化，没得说
+                self._c = _c
+
+            def e():
+                '''
+                    Nature Constant
+                    e = lim_{x \\to 0} ( 1 + \\frac{1}{x} ) ^{x}
+                '''
+                # lim_{x \to 0} ( 1 + \frac{1}{x} ) ^{x} = e
+                __x = 2**52
+                __e = (1 + 1/__x) ** __x
+                return __e
+
+
     class ListSort:
 
             
-        def __init__(self, _any): # 初始化，没得说
-            self.any = _any
+        def __init__(self, _ListSort): # 初始化，没得说
+            self._ListSort = _ListSort
             # self.list_sort = self.ListSort()
-            # self.sys_cmd = self.Sys_cmd()
+            # self.sysCmd = self.SysCmd()
         
 
         def bubble(numList):
@@ -458,7 +654,8 @@ class Tools:
         #         if i not in clist[0]:
         #             clist
 
-    class Sys_cmd:
+
+    class SysCmd:
 
         '''
             系统命令包
@@ -466,8 +663,8 @@ class Tools:
         '''
 
 
-        def __init__(self, _any): # 初始化，没得说
-            self.any = _any
+        def __init__(self, _SysCmd): # 初始化，没得说
+            self._SysCmd = _SysCmd
 
 
         def cmd(command):
@@ -486,7 +683,7 @@ class Tools:
             '''
 
             # os.makedirs(f'.\\{ _file_name }\\')
-            Tools.Sys_cmd.cmd(f'mkdir ".\\{ _file_name }\\"')
+            Tools.SysCmd.cmd(f'mkdir ".\\{ _file_name }\\"')
 
 
         def md(_file_name):
@@ -495,4 +692,4 @@ class Tools:
                 mkdir 的别名
             """
 
-            Tools.Sys_cmd.mkdir(_file_name)
+            Tools.SysCmd.mkdir(_file_name)
